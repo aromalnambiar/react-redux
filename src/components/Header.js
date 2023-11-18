87ecb3200d75438ce16b75d0d274110855ceda38
aromalnambiar <aromalndr@gmail.com>
2023-11-19 00:47:27 +0530

    basic auth using redux-toolkit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,16 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Header.module.css';
+import { authAction } from '../store/store';
 
 const Header = () => {
+
+  const dispatch = useDispatch();
+  const isAuth = useSelector(state => state.auth.isAuth);
+
+  const logout = () => {
+    dispatch(authAction.logout())
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
@@ -12,9 +22,9 @@ const Header = () => {
           <li>
             <a href='/'>My Sales</a>
           </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {isAuth && <li>
+            <button onClick={logout} >Logout</button>
+          </li>}
         </ul>
       </nav>
     </header>
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -2,11 +2,12 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 
-const initialState = {counter: 0, showCounter: false}
+const initialCounterState = {counter: 0, showCounter: false}
+
 
 const counterSlice = createSlice({
     name: 'counter',
-    initialState,
+    initialState: initialCounterState,
     reducers: {
         increament(state){
             state.counter++;
@@ -24,8 +25,28 @@ const counterSlice = createSlice({
 })
 
 
-const store = configureStore({reducer: counterSlice.reducer})
+const initialAuthState = {isAuth: false}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuth = true;
+        },
+        logout(state) {
+            state.isAuth = false;
+        }
+    }
+})
 
 
+const store = configureStore({
+    reducer: {counter: counterSlice.reducer, auth: authSlice.reducer},
+})
+
+
+export const authAction = authSlice.actions;
 export const counterAction = counterSlice.actions;
+
 export default store;
